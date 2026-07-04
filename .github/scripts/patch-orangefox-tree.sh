5c5081da62cd475c289e9a7f1c8ab9d327c1cf67
darkydtm <boat-appear-portal@duck.com>
2026-07-04 15:09:52 +0500

    fix missing uses library patching

--- .github/scripts/patch-orangefox-tree.sh
+++ .github/scripts/patch-orangefox-tree.sh
@@ -23,24 +23,8 @@ fi
 
 echo "Patching ${config_file}"
 
-if ! grep -q "^  ${missing_lib} \\\\" "${config_file}"; then
-	tmp_file="$(mktemp)"
-	if ! awk -v lib="${missing_lib}" '
-		BEGIN { inserted = 0 }
-		/^INTERNAL_PLATFORM_MISSING_USES_LIBRARIES[[:space:]]*:=/ && !inserted {
-			print
-			print "  " lib " \\"
-			inserted = 1
-			next
-		}
-		{ print }
-		END { if (!inserted) exit 1 }
-	' "${config_file}" > "${tmp_file}"; then
-		rm -f "${tmp_file}"
-		echo "Unable to patch INTERNAL_PLATFORM_MISSING_USES_LIBRARIES in ${config_file}" >&2
-		exit 1
-	fi
-	mv "${tmp_file}" "${config_file}"
+if ! grep -Fq "${missing_lib}" "${config_file}"; then
+	printf '\nINTERNAL_PLATFORM_MISSING_USES_LIBRARIES += %s\n' "${missing_lib}" >> "${config_file}"
 fi
 
-grep -A6 "^INTERNAL_PLATFORM_MISSING_USES_LIBRARIES[[:space:]]*:=" "${config_file}"
+grep -n "INTERNAL_PLATFORM_MISSING_USES_LIBRARIES\\|${missing_lib}" "${config_file}"
