e8df987c779db1193b1b57d6ebac690899771ad6
darkydtm <boat-appear-portal@duck.com>
2026-07-05 03:11:49 +0500

    fix recovery script whitespace

--- recovery/root/system/bin/load-mod.sh
+++ recovery/root/system/bin/load-mod.sh
@@ -4,5 +4,3 @@ if /system/bin/grep -Fq twrpfastboot /proc/cmdline; then
     /system/bin/modprobe -d /vendor/lib/modules/1.1 msm_drm
     /system/bin/modprobe -d /vendor/lib/modules msm_drm
 fi
-
-
--- recovery/root/system/bin/variant-script.sh
+++ recovery/root/system/bin/variant-script.sh
@@ -63,5 +63,3 @@ case $project in
 esac
 
 exit 0
-
-
